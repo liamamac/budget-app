3eb4278b33475a65b940157acf43cd183f909692
liamamac <liamalexmackenzie@gmail.com>
2026-04-02 13:50:08 -0400

    initial project setup

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const { MongoClient } = require("mongodb");
+
+let db = null;
+
+async function connectDB() {
+  if (db) return db;
+
+  const client = new MongoClient(process.env.MONGO_URI);
+  await client.connect();
+  console.log("Connected to MongoDB Atlas");
+
+  db = client.db("budgettracker");
+  return db;
+}
+
+module.exports = connectDB;
