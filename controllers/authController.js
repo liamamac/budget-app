22ea60facc99bde125000bc58f55fca782becfd5
liamamac <liamalexmackenzie@gmail.com>
2026-04-05 13:50:42 -0400

    add login function

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const user = require("../models/Users");
+const User = require("../models/Users");
 const bcrpyt = require("bcrypt");
 
 async function register(req, res) {
@@ -19,3 +19,26 @@ async function register(req, res) {
         res.status(500).json({ message: err.message });
     }
 }
+
+async function login(req, res) {
+    try {
+
+        const {email, password} = req.body;
+        const user = await User.findByEmail(email);
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const match = await bcrypt.compare(password, user.passwordHash);
+        if (!match) {
+        return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        res.status(200).json({ message: 'Login successful', userId: user._id });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+module.exports = { register, login };
