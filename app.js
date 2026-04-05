d3d6fbf3d6249cc2b38afadfb3129eaa88102af6
liamamac <liamalexmackenzie@gmail.com>
2026-04-05 14:05:05 -0400

    bug fixed. removed extra period in middleware path

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-app.use('/api/auth', require('../routes/authRoutes'));
+app.use('/api/auth', require('./routes/authRoutes'));
 
 app.get('/api/test', async (req, res) => {
   res.json({ message: 'Connected to backend' });
